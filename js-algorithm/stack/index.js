39a954a55a7fa9f86eebed54777a1680e95ea94b
zhuanghaixin <649308293@qq.com>
2020-09-13 22:39:40 +0800

    算法学习

---
+++ js-algorithm/stack/index.js
@@ -0,0 +1,5 @@
+const stack = [];
+stack.push(2)
+stack.push(1)
+const item1 = stack.pop()
+const item2 = stack.pop()
---
+++ js-algorithm/tree/preorder.js
@@ -0,0 +1,11 @@
+// 二叉树的先序遍历
+
+const bt=require('./bt')
+const preorder=(root)=>{
+    if(!root){return ;}
+    console.log(root.val)
+    preorder(root.left)
+    preorder(root.right)
+}
+
+preorder(bt)
